9d84748d04a5959884729bede45a7d2c63621662
Nastiona <A.N.Dontsova@yandex.ru>
2022-09-17 20:05:34 +0300

    addedExample 06  console project

---
+++ Ex06_Condition_if_else/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введи имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "maria")
+{
+    Console.WriteLine("Ура, это же Maria!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
